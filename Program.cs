5830f1f885fb73127f1e43c31ee2f4334bcaeced
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-01-19 17:54:14 +0400

    Ev Tapsirigi Calculator

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ev_tapsirigi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 15;
+            int num2 = 20;
+            string Calculator = "*";
+            int sum = 0;
+            switch (Calculator)
+            {
+                case "+":
+                    sum = num1 + num2;
+                    break;
+                case "-":
+                    sum = num2 - num1;
+                    break;
+                case "*":
+                    sum = num1 * num2;
+                    break;
+                case "/":
+                    sum = num1 / num2;
+                    break;
+
+            }
+            Console.WriteLine(sum);
+        }
+       }
+}
